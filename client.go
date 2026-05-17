8e891164b84407b77e88e01fe414f79927eae49e
npersily3 <noah.persily@menloschool.org>
2026-05-17 15:07:18 -0700

    Made both client and user way more parrallel (separate initializers, readers, and senders)

--- client.go
+++ client.go
@@ -1,6 +1,35 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
+
+func listen(conn net.Conn) {
+
+}
+
+func readMessage(conn net.Conn) {
+	message := make([]byte, 1024)
+
+	numbytes, err := conn.Read(message)
+
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(message[:numbytes]))
+}
+
+func writeMessage(conn net.Conn, data []byte) {
+	n, err := conn.Write(data)
+	if err != nil {
+		panic(err)
+	}
+	if n != len(data) {
+		panic("short write")
+	}
+}
 
 func client_main() {
 
@@ -12,14 +41,41 @@ func client_main() {
 		} else {
 			break
 		}
-
 	}
 
 	message := make([]byte, 1024)
 
-	conn.Read(message)
+	// wait until this client is initialized in the server
+	for {
+		n, err := conn.Read(message)
 
-	println(string(message))
+		if err != nil {
+			panic(err)
+		}
+		if string(message[:n]) == "initialized" {
+			break
+		}
+	}
 
-	conn.Close()
+	go readMessage(conn)
+
+	for {
+		println("Enter a message")
+
+		length, err := fmt.Scanln(&message)
+
+		if err != nil {
+			panic(err)
+		}
+
+		writeMessage(conn, message[:length])
+
+	}
+
+	err = conn.Close()
+
+	if err != nil {
+		println(err.Error())
+	}
+
 }
--- server.go
+++ server.go
@@ -1,6 +1,89 @@
 package main
 
-import "net"
+import (
+	"encoding/binary"
+	"net"
+)
+
+type ClientInfo struct {
+	conn     net.Conn
+	uniqueId uint32
+	channel  chan []byte
+
+	// have this be last because it is of variable size
+	userName string
+}
+
+type Message struct {
+	senderId uint32
+	contents string
+}
+
+var users = map[uint32]ClientInfo{}
+
+func perClientReader(userId uint32) {
+
+}
+func perClientWriter(userId uint32) {
+
+}
+
+// assumes the first message is a
+func handleConn(conn net.Conn) {
+
+	var clientInfo ClientInfo
+	var rawData []byte
+	n, err := conn.Read(rawData)
+
+	if err != nil {
+		panic(err)
+	}
+	rawData = rawData[:n]
+
+	clientInfo.conn = conn
+	// read the first four bytes
+	id := binary.BigEndian.Uint32(rawData[:4])
+	clientInfo.uniqueId = id
+
+	_, ok := users[clientInfo.uniqueId]
+
+	// if we exist in the hashmap (have been online before
+	if ok {
+
+		// if  we are a new user, initialize, the name and send a message to send a key value of id to name
+	} else {
+		clientInfo.userName = string(rawData[4:n])
+		users[clientInfo.uniqueId] = clientInfo
+
+		//TODO send a key value of name and id for clients to keep
+	}
+
+	//TODO make maybe an enum or some version of a code
+	n, err = conn.Write([]byte("initialized"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	// now that the thread is initialized, we can pull off
+	go perClientWriter(id)
+	go perClientReader(id)
+}
+
+// Constantly listens for new users, and initializes them
+func newUserListener(ln net.Listener) {
+
+	// see if this is always spin or something else
+	for {
+		conn, err := ln.Accept()
+
+		if err != nil {
+			panic(err)
+		}
+		go handleConn(conn)
+
+	}
+}
 
 func server_main() {
 	ln, err := net.Listen("tcp", ":1000")
@@ -9,13 +92,7 @@ func server_main() {
 		panic(err)
 	}
 
-	conn, err := ln.Accept()
+	// initialize all new users
+	go newUserListener(ln)
 
-	if err != nil {
-		panic(err)
-	}
-
-	conn.Write([]byte("hello user, I am server"))
-
-	conn.Close()
 }
